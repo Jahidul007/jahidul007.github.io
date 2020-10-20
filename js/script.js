f1101d06ec2f5b89aef0d607c90436e3c79160b1
Jahidul Islam <jahidul479@gmail.com>
2020-10-20 22:04:57 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,6 @@
 $(document).ready(function() {
 
-	$('#slides').superslides({
-		animation: 'fade',
-		play: 5000,
-		pagination: false
-	});
-
-	var typed = new Typed(".typed", {
-		strings: ["Software Engineer.", "Web Developer.", "Student."],
-		typeSpeed: 70,
-		loop: true,
-		startDelay: 1000,
-		showCursor: false
-	});
+	
 
 	$('.owl-carousel').owlCarousel({
 	    loop:true,
